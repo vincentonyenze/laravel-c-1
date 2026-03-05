38daf5a638bbdeadc3ba3a6c752dcab3b30f0ba7
Vinco Dev <839vinco@gmail.com>
2026-03-05 21:27:54 +0100

    Blade and JavaScript Frameworks

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,16 @@
 <h1>Hello From Laravel Course</h1>
-{{ date('Y') }}
-<p>{{ strtoupper($name . " " . $surname) }}</p>
-<p>{!! $job !!}</p>
-<p>{{ Illuminate\Support\Str::after("Hello World", "Hello") }}</p>
-<p>{{ Illuminate\Foundation\Application::VERSION }}</p>
-<p>{{ PHP_VERSION }}</p>
+@{{ name }}
+
+@@foreach
+
+@verbatim
+    
+<div>
+    Name: {{ name }}
+    Age: {{ age }}
+    Job: {{ job }}
+    Hobbies: {{ hobbies }}
+    @if
+</div>
+
+@endverbatim
