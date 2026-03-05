bd735920806692d0f621cbfe1b421a8cbd2396ea
Vinco Dev <839vinco@gmail.com>
2026-03-05 21:20:50 +0100

    Display Unescaped Data

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,7 @@
 <h1>Hello From Laravel Course</h1>
 {{ date('Y') }}
 <p>{{ strtoupper($name . " " . $surname) }}</p>
+<p>{!! $job !!}</p>
 <p>{{ Illuminate\Support\Str::after("Hello World", "Hello") }}</p>
 <p>{{ Illuminate\Foundation\Application::VERSION }}</p>
 <p>{{ PHP_VERSION }}</p>
