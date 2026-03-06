991f9e1d4c5d21d9954a4fcd9c3a3ee26fd2a55b
Vinco Dev <839vinco@gmail.com>
2026-03-06 23:54:52 +0100

    creating Signup Page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('/about', 'about')->name('about');
+Route::get('/signup', [SignupController::class,'create']);
 
