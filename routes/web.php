ba6c3d40aa616ec18aad7a4ce2916a13a6fb8a89
Vinco Dev <839vinco@gmail.com>
2026-03-07 00:12:36 +0100

    creating login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginCOntroller;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,5 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('/about', 'about')->name('about');
 Route::get('/signup', [SignupController::class,'create']);
+Route::get('/login', [LoginCOntroller::class,'create']);
 
