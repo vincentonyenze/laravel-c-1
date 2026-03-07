4f4645475eb2b791aff76c960013822d1d2c011f
Vinco Dev <839vinco@gmail.com>
2026-03-07 17:06:27 +0100

    Components Slots

--- resources/views/components/admin/card.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="card">
-admin Card components
-</div>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,9 @@
 <div class="card">
-admin Card components
+    <div class="card-header">{{ $title }}</div>
+    @if ($slot->isEmpty())
+        <p>Please provide some content</p>
+        @else 
+        {{ $slot }}
+    @endif
+<div class="card-footer">{{ $footer }}</div>
 </div>
